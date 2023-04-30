9c5f7e651c4930d1087f65323c52864c0e81d67a
gagangani13 <127178242+gagangani13@users.noreply.github.com>
2023-04-30 13:07:49 +0530

    Implementing Login

--- AuthForm.js
+++ AuthForm.js
@@ -6,7 +6,7 @@ const AuthForm = () => {
   const emailRef = useRef();
   const passwordRef = useRef();
   const [isLogin, setIsLogin] = useState(true);
-  const[message,setMessage]=useState(false)
+  const [message, setMessage] = useState(false);
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
@@ -14,40 +14,41 @@ const AuthForm = () => {
 
   async function fetchServer(e) {
     e.preventDefault();
-    setMessage(true)
-    const Email=emailRef.current.value
-    const Password=passwordRef.current.value
-    if(isLogin){
-      console.log('login')
-    }else{
-      const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBXPzqlI6fvUIQX7LiIqUK-vdC_dfWQ0q8',
-        {
-          method: "POST",
-          body: JSON.stringify({
-            email: Email,
-            password: Password,
-            returnSecureToken: true,
-          }),
-          headers: {
-            "Content-Type": "application/json"
-          }
-        }
-      );
-      try {
-        setMessage(false)
-        if(response.ok){
-          console.log(response.json())
-        }else{
-          throw new Error('EMAIL EXISTS')
-        }
-      } catch (error) {
-        const data=await response.json()
-        let msg='ERROR'
-        console.log(data)
-        if(data&&data.error&&data.error.message)
-          msg=data.error.message
-          alert(msg)
+    setMessage(true);
+    const Email = emailRef.current.value;
+    const Password = passwordRef.current.value;
+    let url;
+    if (isLogin) {
+       url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBXPzqlI6fvUIQX7LiIqUK-vdC_dfWQ0q8";
+    } else {
+       url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBXPzqlI6fvUIQX7LiIqUK-vdC_dfWQ0q8";
+    }
+    const response = await fetch(url, {
+      method: "POST",
+      body: JSON.stringify({
+        email: Email,
+        password: Password,
+        returnSecureToken: true,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    try {
+      setMessage(false);
+      if (response.ok) {
+        console.log(response.json());
+      } else {
+        throw new Error("EMAIL EXISTS");
       }
+    } catch (error) {
+      const data = await response.json();
+      let msg = "ERROR";
+      console.log(data);
+      if (data && data.error && data.error.message) msg = data.error.message;
+      alert(msg);
     }
   }
 
@@ -60,15 +61,13 @@ const AuthForm = () => {
           <input type="email" id="email" ref={emailRef} required />
         </div>
         <div className={classes.control}>
-          <label htmlFor="password" >
-            Your Password
-          </label>
-          <input type="password" id="password" required ref={passwordRef}/>
+          <label htmlFor="password">Your Password</label>
+          <input type="password" id="password" required ref={passwordRef} />
         </div>
-        {!message && <button type="submit" >
-          {isLogin ? "LOGIN" : "CREATE ACCOUNT"}
-        </button>}
-        {message && 'Sending request...'}
+        {!message && (
+          <button type="submit">{isLogin ? "LOGIN" : "CREATE ACCOUNT"}</button>
+        )}
+        {message && "Sending request..."}
         <div className={classes.actions}>
           <button
             type="button"
